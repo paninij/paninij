cd29de209d4549d397e926c71d401043c1140340
David Johnston <dwtj@iastate.edu>
2015-02-06 01:12:30 -0600

    Drafted initial code to generate procedures.

--- src/main/java/me/dwtj/capsules/CapsuleGenerator.java
+++ src/main/java/me/dwtj/capsules/CapsuleGenerator.java
@@ -1,6 +1,9 @@
 package me.dwtj.capsules;
 
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -11,8 +14,11 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic.Kind;
 import javax.tools.JavaFileObject;
@@ -64,7 +70,7 @@ public class CapsuleGenerator extends AbstractProcessor
      */
     private void processCapsule(TypeElement elem, RoundEnvironment roundEnv)
     {
-        compileCapsule(elem, roundEnv);
+        makeCapsuleFile(elem, roundEnv);
     }
 
 
@@ -77,36 +83,156 @@ public class CapsuleGenerator extends AbstractProcessor
         // TODO: Also double-check that the element is actually annotated with
         // `@Capsule`.
         // TODO: give warnings/errors when the user annotates some element which cannot be a capsule.
+        // TODO: check that the class does not have any inner classes.
         return elem.getKind() == ElementKind.CLASS;
     }
     
 
-    private void compileCapsule(TypeElement elem, RoundEnvironment roundEnv)
+    private void makeCapsuleFile(TypeElement elem, RoundEnvironment roundEnv)
     {
         Elements utils = processingEnv.getElementUtils();
-        Name pkg = utils.getPackageOf(elem).getQualifiedName();
-        String capsule = elem.getSimpleName() + "Capsule";
-        String qualifiedCapsule = pkg + "." + capsule;
 
-        processingEnv.getMessager().printMessage(Kind.NOTE, "processCapsule()");
+        Name pkg = utils.getPackageOf(elem).getQualifiedName();
         
-		String src = "package " + pkg + ";\n"
-		           + "\n"
-			       + "public class " + capsule + " {\n"
-			       + "}";
-	
+        String src = "package {0};\n"
+                     + "\n"
+                     + "{1}";
+
+        src = MessageFormat.format(src, pkg, buildCapsule(elem, roundEnv));
+
         JavaFileObject file = null;
         try {
-
-            file = processingEnv.getFiler().createSourceFile(qualifiedCapsule);
+            String capsuleClass = pkg + "." + elem.getSimpleName() + "Capsule";
+            file = processingEnv.getFiler().createSourceFile(capsuleClass);
             file.openWriter().append(src).close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
+    /**
+     * @param origElem A handle to the original class from which a capsule is being
+     * built.
+     * @param roundEnv The environment in which the capsule is being built.
+     * @return The source code for the capsule class which has been build to wrap the
+     * original class.
+     */
+    private String buildCapsule(TypeElement orig, RoundEnvironment roundEnv)
+    {
+        Elements utils = processingEnv.getElementUtils();
+        Name pkg = utils.getPackageOf(orig).getQualifiedName();
+        String qualifiedOrig = pkg + "." + orig.getSimpleName();
+
+        processingEnv.getMessager().printMessage(Kind.NOTE, "processCapsule()");
+
+        // Note that `MessageFormat.format()` cannot be used here because of the curly-braces.
+        return buildCapsuleComment(qualifiedOrig)
+             + buildCapsuleDecl(orig, roundEnv) + "\n"
+             + "{\n"
+             + buildCapsuleBody(orig, roundEnv)
+             + "}\n"
+             + "\n";
+    }
     
-    /* Helper Methods *******************************************************/
+
+    private String buildCapsuleComment(String qualifiedOriginal)
+    {
+        String comment = "/**\n"
+                       + " * This class was auto-generated using the `@Capsule` annotation processor from\n"
+                       + " * `{0}`\n"
+                       + " */\n";
+        return MessageFormat.format(comment, qualifiedOriginal);
+    }
+    
+    private String buildCapsuleDecl(TypeElement origElem, RoundEnvironment roundEnv)
+    {
+        Name origName = origElem.getSimpleName();
+        return MessageFormat.format("public class {0}Capsule extends {1}", origName, origName);
+    }
+
+    private String buildCapsuleBody(TypeElement orig, RoundEnvironment roundEnv)
+    {
+        String rv = "";
+
+        for (Element child : orig.getEnclosedElements()) {
+            // For now, ignore everything except for constructors and methods
+            // which need to be wrapped.
+            if (child.getKind() == ElementKind.CONSTRUCTOR) {
+                // TODO
+                rv += "// TODO: build constructor\n\n";
+            } else if (needsProceedureWrapper(child)) {
+                rv += buildProcedure((ExecutableElement) child, roundEnv);
+            }
+        }
+        
+        return rv;
+    }
+    
+    private String buildProcedure(ExecutableElement method, RoundEnvironment roundEnv)
+    {
+        // Note that `MessageFormat.format()` cannot be used here because of the curly-braces.
+        return buildProcedureDecl(method, roundEnv) + "\n"
+             + "{\n"
+             + buildProcedureBody(method, roundEnv)
+             + "}\n"
+             + "\n";
+    }
+    
+    
+    private String buildProcedureDecl(ExecutableElement method, RoundEnvironment roundEnv)
+    {
+        return MessageFormat.format("public {0} {1}Proc({2})",
+                                    method.getReturnType(),
+                                    method.getSimpleName(),
+                                    buildProcedureParameters(method, roundEnv));
+    }
+    
+    
+    private String buildProcedureParameters(ExecutableElement method, RoundEnvironment roundEnv)
+    {
+        List<? extends VariableElement> params = method.getParameters();
+        List<String> paramStrings = new ArrayList<String>(params.size());
+        for (VariableElement param : params) {
+            paramStrings.add(param.toString());
+        }
+        return String.join(", ", paramStrings);
+    }
+    
+    
+    /**
+     * 
+     * @param method The method which the proceedure being build is wrapping.
+     * @param roundEnv
+     * @return
+     */
+    private String buildProcedureBody(ExecutableElement method, RoundEnvironment roundEnv)
+    {
+        return "// TODO: buildProcedureBody()\n";
+
+        //throw new UnsupportedOperationException();
+    }
+    
+    
+    
+    /* Helper Methods ********************************************************/
+    
+    /**
+     * @param elem
+     * @return `true` iff the given `elem` is a method which needs to be 
+     * wrapped as a procedure.
+     */
+    private boolean needsProceedureWrapper(Element elem)
+    {
+        if (elem.getKind() == ElementKind.METHOD) {
+            ExecutableElement method = (ExecutableElement) elem;
+            Set<Modifier> modifiers = method.getModifiers();
+            if (modifiers.contains(Modifier.PUBLIC)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     private TypeElement getTypeElement(String className) {
         return processingEnv.getElementUtils().getTypeElement(className);
