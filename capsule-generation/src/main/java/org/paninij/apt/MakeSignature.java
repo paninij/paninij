fef4a44924a4c37d3e12b6f08c6b0f5abbaea448
dmills <dmills@iastate.edu>
2015-02-24 22:10:39 -0600

    added a class that builds Signatures

---
+++ capsule-generation/src/main/java/org/paninij/apt/MakeSignature.java
@@ -0,0 +1,66 @@
+package org.paninij.apt;
+
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+import org.paninij.apt.util.Source;
+
+public class MakeSignature {
+    TypeElement template;
+    PaniniPress context;
+    
+    static MakeSignature make(PaniniPress context, TypeElement template) {
+        MakeSignature sig = new MakeSignature();
+        sig.context = context;
+        sig.template = template;
+        return sig;
+    }
+    
+    String buildSignature() {
+        String pkg = buildPackage();
+        String src = Source.lines(0, "package #0",
+                                     "",
+                                     "#1",
+                                     "",
+                                     "/**",
+                                     " * This signature was auto-generated from `#2`",
+                                     " */",
+                                     "#3",
+                                     "{",
+                                     "#4",
+                                     "}");
+        return Source.format(src, pkg,
+                                  buildSignatureImports(),
+                                  pkg + "." + template.getSimpleName(),
+                                  buildSignatureDecl(),
+                                  buildSignatureBody());
+    }
+    
+    
+    
+    String buildPackage() {
+        return context.getPackageOf(template);
+    }
+    
+    String buildSignatureBody() {
+        // TODO
+        return "";
+    }
+    String buildSignatureImports() {
+        // TODO
+        return "";
+    }
+    
+    String buildSignatureName() {
+        return template.getSimpleName() + "$Signature";
+    }
+    
+    String buildSignatureDecl() {
+        return "public interface " + buildSignatureName();
+    }
+    
+    String buildMethod(ExecutableElement method) {
+        
+        return "";
+    }
+}
