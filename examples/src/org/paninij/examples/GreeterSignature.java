b062af2ca654a1b447b1c210245f3b7695e59a52
David Johnston <dwtj@iastate.edu>
2015-03-08 14:48:46 -0500

    Removed template classes from `examples` pkg.
    These examples like these should now be put in `examples.helloworld` and
    `examples.duckshapes` as appropriate.

--- examples/src/org/paninij/examples/GreeterSignature.java
+++
@@ -1,9 +0,0 @@
-package org.paninij.examples;
-
-import org.paninij.lang.Signature;
-
-@Signature
-public interface GreeterSignature {
-    String greet();
-    void setGreeting(String greeting);
-}
