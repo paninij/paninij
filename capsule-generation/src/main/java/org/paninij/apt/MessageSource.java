7be85fea083d4d885fcd7b605e2b50bd3db82b9b
dmills <dmills@iastate.edu>
2015-06-01 10:25:52 -0500

    Added license to new files

--- capsule-generation/src/main/java/org/paninij/apt/MessageSource.java
+++ capsule-generation/src/main/java/org/paninij/apt/MessageSource.java
@@ -1,25 +1,33 @@
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ *
+ * Contributor(s): Dalton Mills
+ */
 package org.paninij.apt;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
-import javax.lang.model.type.TypeMirror;
 
-import org.paninij.apt.util.DuckShape;
-import org.paninij.apt.util.JavaModelInfo;
 import org.paninij.apt.util.MessageShape;
-import org.paninij.apt.util.PaniniModelInfo;
 import org.paninij.apt.util.Source;
-import org.paninij.apt.util.DuckShape.Category;
 import org.paninij.apt.util.SourceFile;
-import org.paninij.model.AnnotationKind;
-import org.paninij.model.Capsule;
-import org.paninij.model.MessageKind;
 import org.paninij.model.Procedure;
 import org.paninij.model.Type;
 import org.paninij.model.Variable;
