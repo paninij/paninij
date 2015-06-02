fe1bd32a92fd7277864186bf4deae0165efeb0ee
dmills <dmills@iastate.edu>
2015-06-02 10:35:34 -0500

    added license

--- examples/src/org/paninij/examples/asteroids/InputTemplate.java
+++ examples/src/org/paninij/examples/asteroids/InputTemplate.java
@@ -1,8 +1,24 @@
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ *
+ * Contributor(s): Dalton Mills, Hridesh Rajan
+ */
 package org.paninij.examples.asteroids;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.paninij.lang.Capsule;
 import org.paninij.lang.Wired;
@@ -11,11 +27,6 @@ import org.paninij.lang.Wired;
 public class InputTemplate {
 
     @Wired Ship ship;
-    private BufferedReader in;
-
-    public void init() {
-        this.in = new BufferedReader(new InputStreamReader(System.in));
-    }
 
     public void run() {
         try {
@@ -33,7 +44,6 @@ public class InputTemplate {
                     break;
                 }
             }
-            this.in.close();
         } catch (IOException ioe) {}
     }
 }
