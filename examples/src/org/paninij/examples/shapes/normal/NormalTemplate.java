37859bf69ff2970e0350c21a3baaccf74451d616
dmills <dmills@iastate.edu>
2015-06-05 10:53:17 -0500

    reorganizing example packages

---
+++ examples/src/org/paninij/examples/shapes/normal/NormalTemplate.java
@@ -0,0 +1,25 @@
+package org.paninij.examples.shapes.normal;
+
+import org.paninij.lang.Capsule;
+
+
+@Capsule
+public class NormalTemplate
+{
+    public Object foo(Object o, Object i) {
+        return new Object();
+    }
+
+    public Object primitiveArg(int i)
+    {
+        return new Object();
+    }
+
+    public Object arrayArg(Object[] arr) {
+        return new Object();
+    }
+
+    public Object primitiveArrayArg(int[] arr) {
+        return new Object();
+    }
+}
