7e74668867e2cc9fea839f1539acb6b7fc98cf9f
David Johnston <dwtj@iastate.edu>
2015-07-07 01:07:19 -0500

    Commented-out `List` inside of `Secret`.
    For some reason, having the list inside of `Secret` causes all analyses
    of `LeakyServer` to fail.

--- at-paninij-soter/src/test/java/org/paninij/soter/Secret.java
+++ at-paninij-soter/src/test/java/org/paninij/soter/Secret.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 public class Secret
 {
+    int secret = 42;
+    /*
     List<String> messages = new ArrayList<String>();
 
     public Secret() {
         messages.add("Hello World");
     }
+    */
 }
