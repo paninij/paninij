d0c5870be929a0ec528f07140585558abfb65fa4
dmills <dmills@iastate.edu>
2015-07-20 11:18:48 -0500

    added monitor version benchmark to savina

---
+++ at-paninij-benchmarks/at-paninij-benchmarks-savina-harness/src/main/java/edu/rice/habanero/benchmarks/trapezoid/TrapezoidalAtPaniniJMonitorBenchmark.java
@@ -0,0 +1,36 @@
+package edu.rice.habanero.benchmarks.trapezoid;
+
+import java.io.IOException;
+
+import edu.rice.habanero.benchmarks.Benchmark;
+import edu.rice.habanero.benchmarks.BenchmarkRunner;
+
+public class TrapezoidalAtPaniniJMonitorBenchmark
+{
+    static class TrapezoidalAtPaniniJ extends Benchmark {
+
+        @Override
+        public void cleanupIteration(boolean arg0, double arg1) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void initialize(String[] arg0) throws IOException {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void printArgInfo() {
+            TrapezoidalConfig.printArgs();
+        }
+
+        @Override
+        public void runIteration() {
+            Trapezoid$Monitor.main(null);
+        }
+    }
+
+    public static void main(String[] args) {
+        BenchmarkRunner.runBenchmark(args, new TrapezoidalAtPaniniJ());
+    }
+}
