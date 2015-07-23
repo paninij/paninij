5349f6aa44115617a1666086dd35b3c22db0587e
David Johnston <dwtj@iastate.edu>
2015-07-23 02:04:21 -0500

    Added transfer functions of `LocalLiveAnalysis`.
    This reimplements the transfer function provider that is a part of the
    original SOTER source code. This commit removes the last SOTER
    dependency from the new analysis code, however, the prototype still uses
    SOTER heavily.

--- at-paninij-core/at-paninij-soter/src/main/java/org/paninij/soter/live/LocalLiveAnalysis.java
+++ at-paninij-core/at-paninij-soter/src/main/java/org/paninij/soter/live/LocalLiveAnalysis.java
@@ -1,30 +1,37 @@
 package org.paninij.soter.live;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.Stack;
 
 import org.paninij.soter.cga.CallGraphAnalysis;
 import org.paninij.soter.util.Analysis;
 
+import com.ibm.wala.dataflow.graph.AbstractMeetOperator;
 import com.ibm.wala.dataflow.graph.BitVectorFramework;
+import com.ibm.wala.dataflow.graph.BitVectorKillGen;
 import com.ibm.wala.dataflow.graph.BitVectorSolver;
+import com.ibm.wala.dataflow.graph.BitVectorUnion;
 import com.ibm.wala.dataflow.graph.ITransferFunctionProvider;
 import com.ibm.wala.fixpoint.BitVectorVariable;
+import com.ibm.wala.fixpoint.UnaryOperator;
 import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.propagation.PointerKey;
 import com.ibm.wala.ssa.ISSABasicBlock;
 import com.ibm.wala.ssa.SSACFG;
+import com.ibm.wala.ssa.SSAInstruction;
 import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.NullProgressMonitor;
 import com.ibm.wala.util.MonitorUtil.IProgressMonitor;
 import com.ibm.wala.util.graph.Graph;
 import com.ibm.wala.util.graph.impl.GraphInverter;
+import com.ibm.wala.util.intset.BitVector;
 import com.ibm.wala.util.intset.IntIterator;
 import com.ibm.wala.util.intset.IntSet;
 import com.ibm.wala.util.intset.MutableMapping;
 import com.ibm.wala.util.intset.OrdinalSetMapping;
 
-import edu.illinois.soter.analysis.transferfunctionproviders.LocalLiveVariablesTransferFunctionProvider;
 
 public class LocalLiveAnalysis extends Analysis
 {
@@ -43,9 +50,9 @@ public class LocalLiveAnalysis extends Analysis
         this.cga = cga;
 
         latticeValues = MutableMapping.make();
-        transferFunctions = new LocalLiveVariablesTransferFunctionProvider(cga.getHeapModel(),
-                                                                           node, latticeValues);
+        transferFunctions = new TransferFunctionProvider();
     }
+
 
     @Override
     public void performAnalysis()
@@ -63,8 +70,8 @@ public class LocalLiveAnalysis extends Analysis
             throw new RuntimeException(msg);
         }
     }
-    
-    
+
+
     /**
      * @param basicBlock A basic block in the CG node over which this analysis was performed.
      * 
@@ -92,16 +99,94 @@ public class LocalLiveAnalysis extends Analysis
 
         return pointerKeys;
     }
-    
+
+
     public BitVectorSolver<ISSABasicBlock> getDataFlowSolver()
     {
         assert hasBeenPerformed;
         return dataFlowSolver;
     }
-    
+
+
     public PointerKey getPointerKey(int value)
     {
         assert hasBeenPerformed;
         return latticeValues.getMappedObject(value);
+    }
+    
+     
+    private class TransferFunctionProvider implements ITransferFunctionProvider<ISSABasicBlock, BitVectorVariable>
+    {
+        @Override
+        public AbstractMeetOperator<BitVectorVariable> getMeetOperator()
+        {
+            return BitVectorUnion.instance();
+        }
+
+        @Override
+        public boolean hasEdgeTransferFunctions()
+        {
+            return false;
+        }
+
+        @Override
+        public UnaryOperator<BitVectorVariable> getEdgeTransferFunction(ISSABasicBlock src,
+                                                                        ISSABasicBlock dst)
+        {
+            String msg = "This transfer function provider doesn't provide node transfer functions.";
+            throw new UnsupportedOperationException(msg);
+        }
+
+        @Override
+        public boolean hasNodeTransferFunctions()
+        {
+            return true;
+        }
+
+        @Override
+        public UnaryOperator<BitVectorVariable> getNodeTransferFunction(ISSABasicBlock node)
+        {
+            BitVector gen = new BitVector();
+            BitVector kill = new BitVector();
+            
+            // Process instructions in reverse order.
+            Stack<SSAInstruction> instructions = new Stack<SSAInstruction>();
+            Iterator<SSAInstruction> instrIter = node.iterator();
+            while (instrIter.hasNext()) {
+                instructions.add(instrIter.next());
+            }
+            while (! instructions.isEmpty())
+            {
+                SSAInstruction instr = instructions.pop();
+
+                for (int idx = 0; idx < instr.getNumberOfDefs(); idx++)
+                {
+                    int def = instr.getDef(idx);
+                    int latticeValue = getLatticeValueFromLocalValueNumber(def);
+                    kill.set(latticeValue);
+                    gen.clear(latticeValue);
+                }
+
+                for (int idx = 0; idx < instr.getNumberOfUses(); idx++)
+                {
+                    int use = instr.getUse(idx);
+                    int latticeValue = getLatticeValueFromLocalValueNumber(use);
+
+                    if (use != -1)
+                    {
+                        gen.set(latticeValue);
+                        kill.clear(latticeValue);
+                    }
+                }
+            }
+            
+            return new BitVectorKillGen(kill, gen);
+        }
+        
+        private int getLatticeValueFromLocalValueNumber(int valueNumber)
+        {
+            PointerKey ptr = cga.getHeapModel().getPointerKeyForLocal(node, valueNumber);
+            return latticeValues.add(ptr);
+        }
     }
 }
