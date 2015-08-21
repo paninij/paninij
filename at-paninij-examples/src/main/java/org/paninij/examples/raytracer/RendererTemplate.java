489dc154bde771db9a359c2c2d6c6992126847af
dmills <dmills@iastate.edu>
2015-08-21 11:25:53 -0500

    scene now gets rendered

--- at-paninij-examples/src/main/java/org/paninij/examples/raytracer/RendererTemplate.java
+++ at-paninij-examples/src/main/java/org/paninij/examples/raytracer/RendererTemplate.java
@@ -23,29 +23,24 @@ import org.paninij.lang.Child;
 
     public void design(Renderer self) {
         for (int i = 0; i < tracers.length; i++) {
-            System.out.println("wire " + i + " " + chunkHeight + " " + screenWidth + " " + screenHeight);
             tracers[i].wire(i, chunkHeight, screenWidth, screenHeight);
         }
     }
 
     public Image render(Scene scene) {
+
         BufferedImage image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
 
-        List<Pixel[]> pixels = new ArrayList<Pixel[]>();
+        List<Pixel[]> chunks = new ArrayList<Pixel[]>();
 
+        for (Tracer tracer : tracers) chunks.add(tracer.renderChunk(scene));
 
-        for (Tracer tracer : tracers) pixels.add(tracer.renderChunk(scene));
-
-        System.out.println("done creating image");
-
-        for (Pixel[] chunk : pixels) {
-            for (Pixel p : chunk) {
-                System.out.println("Chunk... " + p.x() + ", " + p.y() + " = " + p.color());
+        for (Pixel[] chunk : chunks) {
+            for (int i = 0; i < chunk.length; i ++) {
+                Pixel p = chunk[i];
                 image.setRGB(p.x(), p.y(), p.color().toRGB());
             }
         }
-
-
 
         return new Image(image);
     }
