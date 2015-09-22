c676dfb90000e50187d44ef3e3cd86cadd130767
David Johnston <dwtj@iastate.edu>
2015-09-22 15:14:50 -0500

    Add `SoterCommand` stub class.

---
+++ at-paninij-analyses/at-paninij-soter/src/main/java/org/paninij/soter/cli/SoterCommand.java
@@ -0,0 +1,17 @@
+package org.paninij.soter.cli;
+
+import org.paninij.soter.util.Command;
+
+public class SoterCommand extends Command
+{
+    public SoterCommand()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void performCommand()
+    {
+        // TODO Auto-generated method stub
+    }
+}
