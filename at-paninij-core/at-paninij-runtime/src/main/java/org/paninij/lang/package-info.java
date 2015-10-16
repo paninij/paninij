e2e14078f95d0bbade0f5e75ef8136b0141e5027
ddmills <dmills@iastate.edu>
2015-10-16 13:37:30 -0500

    added package-info.java (javadoc at package level)

---
+++ at-paninij-core/at-paninij-runtime/src/main/java/org/paninij/lang/package-info.java
@@ -0,0 +1,25 @@
+/**
+ * The org.paninij.lang package offers various classes and annotations which
+ * are used by a developer to create a Panini program. 
+ * 
+ * The primary driver behind @PaniniJ is the annotations within this package.
+ * These include:
+ * <ul>
+ * <li>@Capsule - to create a Capsule Template</li>
+ * <li>@Signature - to create a Signature Template</li>
+ * <li>@Imports - to declare imported fields on a capsule</li>
+ * <li>@Local - to declare local fields on a capsule</li>
+ * <li>@Block - to declare a procedure as 'blocking' behavior</li>
+ * <li>@Future - to declare a procedure as 'futurized' behavior</li>
+ * <li>@Duck - to declare a procedure as 'ducked' behavior</li>
+ * </ul>
+ * 
+ * @see Capsule
+ * @see Signature
+ * @see Imports
+ * @see Local
+ * @see Block
+ * @see Future
+ * @see Duck
+ */
+package org.paninij.lang;
