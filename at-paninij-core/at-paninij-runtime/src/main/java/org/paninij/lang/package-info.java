369160b0d1939da534648bb5d7d939b925d0836d
ddmills <dmills@iastate.edu>
2015-10-27 19:36:38 -0500

    updated examples in package info documentation

--- at-paninij-core/at-paninij-runtime/src/main/java/org/paninij/lang/package-info.java
+++ at-paninij-core/at-paninij-runtime/src/main/java/org/paninij/lang/package-info.java
@@ -1,47 +1,80 @@
 /**
- * <p>The org.paninij.lang package offers various classes and annotations which
- * are used by a developer to create a Panini program.</p>
+ * <p>The org.paninij.lang package offers various classes and annotations
+ * which are used by a developer to create a Panini program.</p>
  * 
  * <h2>Example 1: Creating a capsule</h2>
  * <p><blockquote><pre>
- * &#64;Capsule public class HelloWorldTemplate {
+ * package example;
+ * 
+ * import org.paninij.lang.*;
+ * 
+ * &#64;Root
+ * &#64;Capsule
+ * public class HelloWorldTemplate {
  *     public void run() {
  *         System.out.println("Hello world");
+ *     }
+ *     
+ *     public static void main(String[] args) {
+ *         CapsuleSystem.start("example.HelloWorld", args);
  *     }
  * }
  * </pre></blockquote></p>
  * 
  * <p>Example 1 creates a Capsule Template called HelloWorld. A capsule is a 
  * modular component that encapsulates state and behavior. A Capsule is not 
- * allowed to share it's state with others. Because of the <code>run</code> 
- * method, this is an <em>active</em> capsule. The <code>run</code> method
- * is the main point of entry for this system. </p>
+ * allowed to share it's state with others. Because of the <code>&#64;Root
+ * </code> annotation and the <code>run</code> method, this is an
+ * <em>active</em> capsule. The <code>run</code> method is the main point of
+ * entry for this system.</p>
  * 
  * <h2>Example 2: A system of capsules</h2>
+ * <h4>ConsoleTemplate.java</h4>
  * <p><blockquote><pre>
- * &#64;Capsule public class ConsoleTemplate {
+ * package example;
+ * 
+ * import org.paninij.lang.*;
+ * 
+ * &#64;Capsule
+ * public class ConsoleTemplate {
  *     public void say(String message) {
  *         System.out.println(message);
  *     }
  * }
+ * </pre></blockquote></p>
  * 
- * &#64;Capsule public class GreeterTemplate {
+ * <h4>GreeterTemplate.java</h4>
+ * <p><blockquote><pre>
+ * package example;
+ * 
+ * import org.paninij.lang.*;
+ * 
+ * &#64;Root
+ * &#64;Capsule
+ * public class GreeterTemplate {
  *     &#64;Local Console console;
  *     
  *     public void run() {
  *         console.say("Hello world");
  *     }
+ *     
+ *     public static void main(String[] args) {
+ *         CapsuleSystem.start("example.Greeter", args);
+ *     }
  * }
- * </pre></blockquote></p> 
- * <p>Example 2 creates two capsules, Greeter and Console. The Console capsule
- * has one <em>procedure</em> called <code>say</code>. The Greeter capsule
- * creates a Console called "console" (<code>&#64;Local Console console</code>
- * ). Note that the Greeter capsule is an <em>active</em> capsule because it
- * contains a <code>run</code> method. Inside the <code>run</code> method, we 
- * invoke the <code>say</code> procedure on the instance of <code>Console</code>
- * with a message.</p>
+ * </pre></blockquote></p>
  * 
- * <p>The primary driver behind @PaniniJ is the annotations within this package.</p>
+ * <p>Example 2 creates two capsules, Greeter and Console. The Console
+ * capsule has one <em>procedure</em> called <code>say</code>. The
+ * Greeter capsule creates a Console called "console" (<code>&#64;Local
+ * Console console</code>). Note that the Greeter capsule is an
+ * <em>active</em> capsule because it contains a <code>run</code>
+ * method. Inside the <code>run</code> method, we invoke the <code>say
+ * </code> procedure on the instance of <code>Console</code>with a
+ * message.</p>
+ * 
+ * <p>The primary driver behind @PaniniJ is the annotations within this
+ * package.</p>
  * 
  * <h3>Annotations used to declare a Capsule Template:</h3>
  * <ul>
