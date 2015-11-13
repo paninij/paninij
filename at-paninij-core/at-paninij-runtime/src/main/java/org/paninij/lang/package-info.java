a8c211d91da55035f98e1a0e4da3109edd08a39b
ddmills <dmills@iastate.edu>
2015-11-13 15:34:59 -0600

    Added license to all java files. Closes #75

--- at-paninij-core/at-paninij-runtime/src/main/java/org/paninij/lang/package-info.java
+++ at-paninij-core/at-paninij-runtime/src/main/java/org/paninij/lang/package-info.java
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * @PaniniJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @PaniniJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with @PaniniJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ *
+ * Contributors:
+ * 	Dr. Hridesh Rajan,
+ * 	Dalton Mills,
+ * 	David Johnston,
+ * 	Trey Erenberger
+ *******************************************************************************/
 /**
  * <p>The <code>org.paninij.lang</code> package offers various classes and annotations which are used by a
  * developer to create an &#64;PaniniJ program. &#64;PaniniJ is an annotation-based realization of capsule-
