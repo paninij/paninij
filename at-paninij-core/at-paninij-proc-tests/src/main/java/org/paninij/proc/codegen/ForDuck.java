ee2535003ca0d2cb3f550cd1c69f197b0fe9d417
jlmaddox <jlmaddox@users.noreply.github.com>
2016-03-09 16:34:51 -0600

    Adds tests for class generation checking

---
+++ at-paninij-core/at-paninij-proc-tests/src/main/java/org/paninij/proc/codegen/ForDuck.java
@@ -0,0 +1,14 @@
+package org.paninij.proc.codegen;
+
+public class ForDuck {
+    private int x, y;
+    
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void print() {
+        System.out.println(x + " " + y);
+    }
+}
