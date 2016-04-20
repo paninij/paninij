29f2d5c799aedbb07638a69d7614389b3564d157
jlmaddox <jlmaddox@users.noreply.github.com>
2016-04-20 15:27:51 -0500

    Merged temporary test project into proc
    - Additionally added the template test sources
    - Refactored bad template tests to fail properly
    - Split bad template tests into one for capsule and signature
    - Removed references to @BadTemplate, which will not exist

---
+++ at-paninij-core/at-paninij-proc/src/test/java/org/paninij/proc/util/AbstractTestBadTemplates.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * @PaniniJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @PaniniJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with @PaniniJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ *
+ * Contributors:
+ *  Dr. Hridesh Rajan,
+ *  Dalton Mills,
+ *  David Johnston,
+ *  Trey Erenberger
+ *  Jackson Maddox
+ *******************************************************************************/
+package org.paninij.proc.util;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+public abstract class AbstractTestBadTemplates extends AbstractCompileTest {
+
+    public AbstractTestBadTemplates(ArrayList<String> classes) {
+        super(classes);
+    }
+    
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
+    @Before
+    public void setUp()
+    {
+        expected.expect(RuntimeException.class);
+        expected.expectCause(instanceOf(getExpectedCause()));
+        // TODO: Figure out if we can specify the expected error source (i.e. the check from which
+        // the exception was originally thrown).
+    }
+
+    protected abstract Class<?> getExpectedCause();
+}
---
+++ at-paninij-core/at-paninij-proc/src/test/sources/org/paninij/proc/check/signature/HasDuckAnnotationTemplate.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * @PaniniJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @PaniniJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with @PaniniJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ *
+ * Contributors:
+ * 	Dr. Hridesh Rajan,
+ * 	Dalton Mills,
+ * 	David Johnston,
+ * 	Trey Erenberger
+ *******************************************************************************/
+package org.paninij.proc.check.signature;
+
+import org.paninij.lang.Duck;
+import org.paninij.lang.Signature;
+
+@Duck
+@Signature
+public interface HasDuckAnnotationTemplate
+{
+    // Nothing needed here
+}
