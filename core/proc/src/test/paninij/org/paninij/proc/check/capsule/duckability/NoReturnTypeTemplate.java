8ecb85cc147d5f7fb1ae29a96ea640c9923294e8
David Johnston <dwtj@iastate.edu>
2016-09-24 15:50:53 -0500

    Add compile test: capsule has @Duck void procedure
    We would expect a capsule check to detect this and to report it
    appropriately, but this is not happening. This was predicted by
    Issue #144. This compile test currently fails, but it should pass once
    Issue #144 is resolved.

---
+++ core/proc/src/test/paninij/org/paninij/proc/check/capsule/duckability/NoReturnTypeTemplate.java
@@ -0,0 +1,11 @@
+package org.paninij.proc.check.capsule.duckability;
+
+import org.paninij.lang.Capsule;
+import org.paninij.lang.Duck;
+
+@Capsule
+public class NoReturnTypeTemplate {
+    @Duck public void proc() {
+        // Nothing needed here.
+    }
+}
