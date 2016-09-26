de551884f2d12e6c8b42a26326b7ba8d2e84491c
David Johnston <dwtj@iastate.edu>
2016-09-25 20:44:10 -0500

    Add compile test for design decls's self arg

---
+++ core/proc/src/test/paninij/org/paninij/proc/decls/DesignWithSelfArgTemplate.java
@@ -0,0 +1,10 @@
+package org.paninij.proc.decls;
+
+import org.paninij.lang.Capsule;
+
+@Capsule
+public class DesignWithSelfArgTemplate {
+    void design(DesignWithSelfArg self) {
+        // Nothing needed here.
+    }
+}
