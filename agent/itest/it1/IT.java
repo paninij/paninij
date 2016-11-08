9f342d140cdf17d790d5e938981714c5bf2589b7
David Johnston <dwtj@iastate.edu>
2016-11-08 15:24:28 -0600

    Refactor agent tests to use Gradle, not CTest
    This change is useful because (1) the implementation is simpler, (2) the
    @PaniniJ annotation processor is now run when an integration test is
    compiled, and (3) it is now much easier to attach to tests with a
    debugger.
    
    IDE-debuggable integration test can now be launched from within my IDE
    or from the command line (using Gradle's `--debug-jvm` switch). Using
    either of these methods, one can use the "double debugger" method often
    used in JNI development, where both a Java debugger and a native
    debugger is attached to a `java` process. In this way, one can step
    through both Java code and native code running in a process.

--- agent/itest/it1/IT.java
+++ agent/itest/it1/IT.java
@@ -1,3 +1,5 @@
+package it1;
+
 public class IT {
     public static void main(String[] args) {
         System.out.println("Hello, from a `main()` method.");
--- agent/itest/it2/IT.java
+++ agent/itest/it2/IT.java
@@ -1,9 +1,16 @@
+package it2;
+
 import org.paninij.runtime.check.Ownership;
 
 public class IT {
     public static void main(String[] args) {
         System.out.println("Hello, from a `main()` method.");
         Object o = new Object();
-        Ownership.move(o, null, o);
+        try {
+            Ownership.move(o, null, o);
+        } catch (Error err) {
+            return;
+        }
+        assert false: "Expected an ownership move error to occur.";
     }
 }
