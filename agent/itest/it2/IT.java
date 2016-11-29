5da6d034c30f721a50a79903f4a1cd9d134ea31f
David Johnston <dwtj@iastate.edu>
2016-11-28 23:27:24 -0600

    Disable and remove body of agent's `it2`
    `Ownership#move()` no longer works in this naive way: it expects
    `Panini$Capsule` instances to be passed in.

--- agent/itest/it2/IT.java
+++ agent/itest/it2/IT.java
@@ -1,16 +1,7 @@
 package it2;
 
-import org.paninij.runtime.check.Ownership;
-
-import static java.lang.System.out;
-import static org.paninij.agent.util.Assert.assertOwnershipError;
-
 public class IT {
     public static void main(String[] args) {
-        out.println("Hello, from a `main()` method.");
-        Object o = new Object();
-        assertOwnershipError(() -> {
-            Ownership.move(o, null, o);
-        });
+        // TODO: Everything
     }
 }
