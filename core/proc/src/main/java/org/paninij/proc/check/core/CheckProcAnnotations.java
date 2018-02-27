4ea7e23045651f3ce9e1171b289f546427516054
Eva Kuntz <evakuntz@iastate.edu>
2018-02-27 11:18:29 -0600

    '#175' add generalized compiler check for 'at-Duck' 'at-Future' 'at-Block'

--- core/proc/src/main/java/org/paninij/proc/check/core/CheckProcAnnotations.java
+++ core/proc/src/main/java/org/paninij/proc/check/core/CheckProcAnnotations.java
@@ -66,78 +66,109 @@ public class CheckProcAnnotations implements CoreCheck
         this.checker = new DuckabilityChecks(procEnv);
     }
 
-	@Override
-	public Result checkCore(TypeElement core, CoreKind coreKind) {
-		for (Element member : core.getEnclosedElements()) {
-			if (isUnannotatedVoidProcedure(member)) {
-				continue; // This kind of procedure is ok, so skip the duckability check on it.
-			}
-			// Check the duckability of a procedure's return type if that procedure is
-			// either
-			// explicitly or implicitly an `@Duck` procedure:
-			if (isDuckProcedure(member)) {
-				Result result = checker.checkDuckability(((ExecutableElement) member).getReturnType());
-				if (!result.ok()) {
-					String err = "A {0} core has a procedure whose return type cannot be " + "ducked. {1}";
-					err = format(err, coreKind, result.errMsg());
-					return error(err, CheckProcAnnotations.class, member);
-				}
-			}
+    @Override
+    public Result checkCore(TypeElement core, CoreKind coreKind) {
+        for (Element member : core.getEnclosedElements()) {
+            if (isUnannotatedVoidProcedure(member)) {
+                continue; // This kind of procedure is ok, so skip the duckability check on it.
+            }
 
-			// check if '@Duck' procedure is also annotated as a '@Future' procedure
-			if (hasAnnotation(member, Future.class) && hasAnnotation(member, Duck.class)) {
-				String err = "A procedure cannot be annotated with both '@Duck' and '@Future'.";
-				Result result = error(err, CheckProcAnnotations.class, member);
-				return result;
+            // Check if procedure contains multiple annotations
+            String error = multipleProcAnnotations(member);
+            if (error != null) {
+                return error(error, CheckProcAnnotations.class, member);
+            }
 
-			}
-		}
-		return OK;
-	}
+            // Check the duckability of a procedure's return type if that procedure is
+            // either
+            // explicitly or implicitly an `@Duck` procedure:
+            if (isDuckProcedure(member)) {
+                Result result = checker.checkDuckability(((ExecutableElement) member).getReturnType());
+                if (!result.ok()) {
+                    String err = "A {0} core has a procedure whose return type cannot be " + "ducked. {1}";
+                    err = format(err, coreKind, result.errMsg());
+                    return error(err, CheckProcAnnotations.class, member);
+                }
+            }
+
+        }
+        return OK;
+    }
 
     /**
-     * Indicates whether a member has none of the PaniniJ procedure annotations and it returns
-     * `void`.
+     * Indicates whether a member has both a Duck and Block, Block and Future, or
+     * Duck and Future annotations.
+     * 
+     * @param member
+     *            Member to check.
+     * @return String with either error message or empty string (null).
+     */
+    private static String multipleProcAnnotations(Element member) {
+        String err = null;
+        if (isMethod(member)) {
+            // check if '@Duck' procedure is also annotated as a '@Future' procedure
+            if (hasAnnotation(member, Future.class) && hasAnnotation(member, Duck.class)) {
+               err = "A procedure cannot be annotated with both '@Duck' and '@Future'.";
+               return err;
+            }
+            // check if '@Duck' procedure is also annotated as a '@Block' procedure
+            if (hasAnnotation(member, Block.class) && hasAnnotation(member, Duck.class)) {
+                err = "A procedure cannot be annotated with both '@Duck' and '@Block'.";
+                return err;
+            }
+            // check if '@Block' procedure is also annotated as a '@Future' procedure
+            if (hasAnnotation(member, Block.class) && hasAnnotation(member, Future.class)) {
+                err = "A procedure cannot be annotated with both '@Block' and '@Future'.";
+                return err;
+            }
+        }
+
+        return err;
+    }
+
+    /**
+     * Indicates whether a member has none of the PaniniJ procedure annotations and
+     * it returns `void`.
      */
     private static boolean isUnannotatedVoidProcedure(Element member) {
         if (member.getKind() != METHOD) {
             return false;
         }
         ExecutableElement method = (ExecutableElement) member;
-        return method.getReturnType().getKind() == VOID
-            && !isDecl(method)
-            && !hasAnnotation(method, Duck.class)
-            && !hasAnnotation(method, Future.class)
-            && !hasAnnotation(method, Block.class);
+        Set<Modifier> modifiers = method.getModifiers();
+        if (method.getReturnType().getKind() == VOID && modifiers.contains(Modifier.STATIC)
+                && modifiers.contains(Modifier.PRIVATE)) {
+            return true;
+        }
+        return method.getReturnType().getKind() == VOID && !isDecl(method) && !hasAnnotation(method, Duck.class)
+                && !hasAnnotation(method, Future.class) && !hasAnnotation(method, Block.class);
     }
 
     /**
-     * Indicates whether a member is either explicitly or implicitly a {@link Duck @Duck}
-     * procedure.
+     * Indicates whether a member is either explicitly or implicitly a
+     * {@link Duck @Duck} procedure.
      */
     private static boolean isDuckProcedure(Element member) {
-        if (member.getKind() != METHOD){
+        if (member.getKind() != METHOD) {
             return false;
         }
-        Set<Modifier> modifiers = ((ExecutableElement)member).getModifiers();
-        
-        return !isDecl(member)
-            && !hasAnnotation(member, Future.class)
-            && !hasAnnotation(member, Block.class)
-            && !modifiers.contains(Modifier.STATIC)
-            && !modifiers.contains(Modifier.PRIVATE);
+        Set<Modifier> modifiers = ((ExecutableElement) member).getModifiers();
+
+        return !isDecl(member) && !modifiers.contains(Modifier.STATIC) && !modifiers.contains(Modifier.PRIVATE);
+    }
+
+    private static boolean isMethod(Element member) {
+        return (member.getKind() != METHOD);
     }
 
     private static boolean isDecl(Element member) {
         return isDeclName(member.getSimpleName().toString());
     }
-    
+
     private static boolean isDeclName(String memberName) {
-        return memberName.equals("init")
-            || memberName.equals("design")
-            || memberName.equals("run");
+        return memberName.equals("init") || memberName.equals("design") || memberName.equals("run");
     }
-    
+
     private static <A extends Annotation> boolean hasAnnotation(Element elem, Class<A> anno) {
         return elem.getAnnotation(anno) != null;
     }
--- core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/ProcedureOnActiveCore.java
+++ core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/ProcedureOnActiveCore.java
@@ -1,15 +1,34 @@
 package org.paninij.proc.check.capsule.procedures;
 
 import org.paninij.lang.Capsule;
+import org.paninij.lang.Future;
+import org.paninij.lang.String;
+import org.paninij.lang.Duck;
+import org.paninij.lang.Future;
+import org.paninij.lang.Block;
 
 @Capsule
-class ProcedureOnActiveCore
-{
+class ProcedureOnActiveCore {
     void procA() {
         // Nothing needed here.
     }
-    
+
     void run() {
         // Nothing needed here.
+    }
+
+    @Duck @Future
+    int methodA() {
+        return 0;
+    }
+
+    @Duck @Block
+    int methodB() {
+        return 0;
+    }
+
+    @Future @Block
+    int methodC() {
+        return 0;
     }
 }
