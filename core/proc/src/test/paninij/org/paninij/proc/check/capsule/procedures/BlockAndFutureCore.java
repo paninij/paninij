73530cd74f7dfbb3075a8c026bb3c597e57c8939
Eva Kuntz <evakuntz@iastate.edu>
2018-03-02 11:39:49 -0600

    #175 adding separate test files for at-Duck at-Block at-Future checks

--- core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/BlockAndFutureCore.java
+++ core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/BlockAndFutureCore.java
@@ -3,14 +3,12 @@ package org.paninij.proc.check.capsule.procedures;
 import org.paninij.lang.Capsule;
 import org.paninij.lang.Future;
 import org.paninij.lang.String;
+import org.paninij.lang.Block;
 
 @Capsule
-class ProcedureOnActiveCore {
-    void procA() {
-        // Nothing needed here.
-    }
-
-    void run() {
-        // Nothing needed here.
+class BlockAndFutureCore {
+    @Block @Future
+    int methodA() {
+        return 0;
     }
 }
