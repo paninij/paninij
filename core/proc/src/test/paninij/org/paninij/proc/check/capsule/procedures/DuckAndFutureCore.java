2492182ed5e2414c0479fa2fdcd10e5d006d6403
Eva Kuntz <evakuntz@iastate.edu>
2018-03-07 11:27:26 -0600

    #175 adding generalized check for duck block future procedures

--- core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/DuckAndFutureCore.java
+++ core/proc/src/test/paninij/org/paninij/proc/check/capsule/procedures/DuckAndFutureCore.java
@@ -3,14 +3,12 @@ package org.paninij.proc.check.capsule.procedures;
 import org.paninij.lang.Capsule;
 import org.paninij.lang.Future;
 import org.paninij.lang.String;
+import org.paninij.lang.Duck;
 
 @Capsule
-class ProcedureOnActiveCore {
-    void procA() {
-        // Nothing needed here.
-    }
-
-    void run() {
-        // Nothing needed here.
+class DuckAndFuture {
+    @Duck @Future
+    int methodA() {
+        return 0;
     }
 }
